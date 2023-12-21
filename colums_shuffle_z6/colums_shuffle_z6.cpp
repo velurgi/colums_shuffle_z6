5563e74db6d20fb2e05efd66beb5327bf87f4a58
Dmitriy Barabkin <barabkin8@gmail.com>
2023-12-21 21:10:28 +0300

    Добавьте файлы проекта.

---
+++ colums_shuffle_z6/colums_shuffle_z6.cpp
@@ -0,0 +1,27 @@
+﻿//Рандом+столбцы
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int dataCheckInt(int a) {
+    while (cin.fail() || a < 1 || a > 2) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Ошибка, вы должны ввести целое число." << endl;
+        cin >> a;
+    }
+    return a;
+}
+
+int main()
+{
+    cout << "Введите размерность матрицы:" << endl;
+    int n = 0;
+    cin >> n;
+    n = dataCheckInt(n);
+
+
+    return 0;
+}
